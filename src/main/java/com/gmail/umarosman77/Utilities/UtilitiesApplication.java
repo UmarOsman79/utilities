6fd8627d24274ca463d48966216c5f7e8f039bf0
Umar Osman <umarosman77@gmail.com>
2025-09-07 23:22:17 +0200

    First push

---
+++ src/main/java/com/gmail/umarosman77/Utilities/UtilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.umarosman77.Utilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/umarosman77/Utilities/UtilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.umarosman77.Utilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
